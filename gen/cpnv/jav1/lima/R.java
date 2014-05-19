7f44791351c81d41621f3d73befb2b744e2e945c
Olivier.NICOLE <Olivier.NICOLE@SC-C333-PC11.sc.cpnv.ch>
2014-05-19 10:30:03 +0200

    Delete log.xml

--- gen/cpnv/jav1/lima/R.java
+++ gen/cpnv/jav1/lima/R.java
@@ -28,15 +28,14 @@ public final class R {
         public static final int cmdMainDistribution=0x7f060009;
         public static final int cmdMainInventory=0x7f060008;
         public static final int cmdMainValidation=0x7f060007;
-        public static final int goAct1=0x7f060002;
-        public static final int goAct2=0x7f060003;
-        public static final int outputzone=0x7f060001;
-        public static final int scrollViewDebug=0x7f060000;
+        public static final int goAct1=0x7f060000;
+        public static final int goAct2=0x7f060001;
+        public static final int outputzone=0x7f060003;
+        public static final int scrollViewDebug=0x7f060002;
     }
     public static final class layout {
         public static final int debug=0x7f030000;
-        public static final int log=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050005;
